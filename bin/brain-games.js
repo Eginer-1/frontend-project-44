de65af69288e31df0f650485be60a44dd67eb3ca
Eginer-1 <lesha.tolstov07@gmail.com>
2025-05-13 19:48:32 +0300

    исправлен файл brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
-const name = readlineSync.question("Your answer: ");
 
 console.log("Welcome to the Brain Games!");
+const name = readlineSync.question("Your answer: ");
 console.log("Hello " + name);
