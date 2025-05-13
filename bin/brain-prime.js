b054f87031a01902c1eddd4708807b6c86cdee51
Eginer-1 <lesha.tolstov07@gmail.com>
2025-05-13 15:21:15 +0300

    сделали последнюю игру

---
+++ bin/brain-prime.js
@@ -0,0 +1,35 @@
+import readlineSync from "readline-sync";
+import askName from "../utils/welcome.js";
+
+let name = askName();
+let numWin = 0;
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const startGame = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  for (let i = 0; i < 3; i++) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question("Your answer: ");
+
+    const correctAnswer = isPrime(number) ? "yes" : "no";
+    if (answer === correctAnswer) {
+      console.log("Correct!");
+    } else {
+      console.log(`Wrong! The correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      startGame();
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+startGame();
